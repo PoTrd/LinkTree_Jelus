95d143b38062ec7f35dbd0997516ec6936dbf15f
PoTrd <paul.taurand@gmail.com>
2022-04-04 12:53:54 +0200

    Premiére version du Linktree (sans contact, ..) et premier versionnage en ligne

---
+++ Index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Jelus-Music's Links</title>
+    <link rel="stylesheet" href="Linktree_jelus.css" />
+</head>
+
+<body>
+    <div id="block_page">
+        <figure><img src="pp_jelus/pp_jelus.png" alt="Photo de profil de Jelus"></figure>
+        <nav>
+            <ul>
+                <li><a href="#">Singles</a></li>
+                <li><a href="#">A propos</a></li>
+                <li><a href="LinkTree Jelus.html">Liens de <br />distribution</a></li>
+            </ul>
+        </nav>
+        <h1>Jelus Music</h1>
+        <div>
+            <div class="lien"><a href="https://open.spotify.com/artist/0kwv85zWzzsevmyyCMPlPC"
+                    target="blank">Spotify</a>
+            </div>
+            <div class="lien"><a href="https://www.deezer.com/fr/artist/142536222" target="blank">Deezer</a></div>
+            <div class="lien"><a href="https://soundcloud.com/user-25948023" target="blank">SoundCloud</a></div>
+            <div class="lien"><a href="https://www.youtube.com/channel/UCsDmgZXH-2TrHnDC-inpUoQ"
+                    target="blank">Youtube</a>
+            </div>
+            <div class="lien"><a href="https://www.instagram.com/jules_trd/" target="blank">Instagram</a></div>
+        </div>
+    </div>
+</body>
+
+</html>
